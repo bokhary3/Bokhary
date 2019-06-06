538066c78459bcf13b4129054df48198431c8681
bokhary <elsayed.husseinn15@gmail.com>
2019-06-06 22:27:23 +0300

    Initial Commit

---
+++ Bokhary/Bokhary.h
@@ -0,0 +1,19 @@
+//
+//  Bokhary.h
+//  Bokhary
+//
+//  Created by Elsayed Hussein on 6/6/19.
+//  Copyright © 2019 Elsayed Hussein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bokhary.
+FOUNDATION_EXPORT double BokharyVersionNumber;
+
+//! Project version string for Bokhary.
+FOUNDATION_EXPORT const unsigned char BokharyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bokhary/PublicHeader.h>
+
+
